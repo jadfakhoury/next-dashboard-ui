a61bfbdff9dcc163fdc6b79489d8fd3784d0c7ae
Jad Fakhoury <jad.fakhoury@exquitech.com>
2024-12-09 20:24:18 +0200

    admin page

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,37 @@
+import CountChart from '@/components/CountChart';
+import UserCard from '@/components/UserCard';
+
 const AdminPage = () => {
-  return <div className=''>Admin Page</div>;
+  return (
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* Left */}
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
+        <div className='flex gap-4 justify-between flex-wrap'>
+          <UserCard type='Student' />
+          <UserCard type='Teacher' />
+          <UserCard type='Parent' />
+          <UserCard type='Staff' />
+        </div>
+
+        {/* Middle Charts */}
+        <div className='flex gap-4 lg:flex-row'>
+          {/* Count Chart */}
+          <div className='w-full lg:w-1/3 h-[450px]'>
+            <CountChart />
+          </div>
+
+          {/* Attendance Chart */}
+          <div className='w-full lg:w-2/3 h-[450px]'></div>
+        </div>
+
+        {/* Bottom Charts */}
+        <div className=''></div>
+      </div>
+
+      {/* Right */}
+      <div className='w-full lg:w-1/3'>r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
